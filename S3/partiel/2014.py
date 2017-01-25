3f079cd6dfbbe3def7d098c972ad941d8b294869
tomMoulard <tom.moulard@epita.fr>
2017-01-25 11:38:06 +0100

    Parcours largeur

--- S3/partiel/2014.py
+++ S3/partiel/2014.py
@@ -1,8 +1,29 @@
 #python3
 #made by moular_b
 import GraphMat
+import queue as Q
 
 
 # exo4
 
+def parc_lag(G, x):
+  marque = [False] * G.order
+  for x in range(G.order):
+    if not marque[x]:
+      largeur(x, G, marque)
+
+def largeur(x, G, marque):
+  marque[x] = True
+  f = Q.Queue()
+  f = Q.enqueue(x, f)
+  while not isEmpty(f):
+    y = Q.dequeue(f)
+    #rencontre de chaques noeuds
+    for i in range(G.adjlist[y]): # i = adj de y
+      #arc y -> x
+      if not marque[i]:
+        marque[i] = True:
+        f = Q.enqueue(i, f)
+
 def isTree(G):
+  
